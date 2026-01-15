2bb1ab49714652878307fdce16990c8dded2f20e
234 <martinkihungi2@gmail.com>
2026-01-15 17:42:54 +0300

    Setup for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/juman-travel/", // <-- important: use your GitHub repo name
   plugins: [react()],
-  base: "/juman-travel/", // <-- crucial for GitHub Pages
 });
